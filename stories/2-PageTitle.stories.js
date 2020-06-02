e48cabfe25f46fabd25ea0a7b4c0e4249f37ea25
Erica <erica@get-local.app>
2020-06-01 19:17:41 -0500

    added a page title story

---
+++ stories/2-PageTitle.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean, withKnobs } from '@storybook/addon-knobs';
+import PageTitle from '../components/template/UiElements/PageTitle/PageTitle';
+
+const stories = storiesOf('UI Elements/Page Title', module);
+stories.addDecorator(withKnobs);
+stories.add('Default', () => (
+  <PageTitle
+    title={text('title', 'Page Title')}
+    subtitle={text('subtitle', 'Subtitle')}
+    backdrop={boolean('show backdrop', false)}
+    bgColor={text('background', 'transparent')}
+  />
+));
